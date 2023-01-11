743446943da863e0cba5241f5804cafc9851c590
Павел <gololobovpd18@mail.ru>
2023-01-11 12:57:25 +0800

    Тест

---
+++ main.py
@@ -0,0 +1,78 @@
+import pygame
+#
+import sys
+import os
+
+
+
+
+FPS = 50
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (1000, 1000))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Движущийся круг 2')
+    size = width, height = 800, 400
+    screen = pygame.display.set_mode(size)
+#
+    running = True
+    v = 20  # пикселей в секунду
+    fps = 60
+    clock = pygame.time.Clock()
+    while running:
+        start_screen()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+    #     pygame.draw.circle(screen, (255, 0, 0), (int(x_pos), 200), 20)
+    #     x_pos += v / fps
+    #     clock.tick(fps)
+        pygame.display.flip()
+    pygame.quit()
+print('hello')
